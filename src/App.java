a3f052943940a107ee4133d34f44ed2120bc0383
Smatheus-dev <m.t.s_@hotmail.com>
2025-05-09 11:08:39 -0300

    ex1-Finalizado

--- src/App.java
+++ src/App.java
@@ -1,28 +1,56 @@
-import javax.swing.JOptionPane;
+  
+    import javax.swing.JOptionPane;
 
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Alô Git!");
 
+    public class App {                                                  
+        public static void main(String[] args) throws Exception {
+            JOptionPane.showMessageDialog(null," Bem - Vindo ao Exercício de Java-Aula 7    "); 
+            JOptionPane.showMessageDialog(null,"            Alô Jardim!          ");
+
+            String nomeCliente = "";
+            String telefone = "";
+            String enderecoCliente = "";
+            boolean cadastrado = false;
         
-                    //  Classe é um conjunto de objetos
-                    //  Nosso objetivo nesse código é entender a diferença entre método com o tipo VOID e com o tipo String 
-                    // Entre método com o tipo void e método com o o tipo String
-                    Mamifero m1 = new Mamifero();
-                    m1.setNomeCientifico("Bos Taurus");
-                    JOptionPane.showMessageDialog(null, "Nome Cientifico"+ "\n" + m1.getNomeCientifico());
-
-                    Mamifero m2 = new Mamifero();
-                    m2.setNomeCientifico("Macropus");
-                    JOptionPane.showMessageDialog(null, "Nome Cientifico"+ "\n" + m2.getNomeCientifico());
-
-                    Mamifero m3 = new Mamifero();
-                    m3.setNomeCientifico("Panthera tigris");
-                    JOptionPane.showMessageDialog(null,  "Nome Cientifico"+ "\n" + m3.getNomeCientifico());
+                while (!cadastrado) {
+                    JOptionPane.showMessageDialog(null, " Por favor, informe seus dados: "); 
                     
-                    Mamifero m4 = new Mamifero();
-                    m4.setNomeCientifico("Loxodonta africana");
-                    JOptionPane.showMessageDialog(null,  "Nome Cientifico"+ "\n" + m4.getNomeCientifico());
+                    nomeCliente = JOptionPane.showInputDialog(null, "Informe seu nome:");
+                        if (nomeCliente == null){
+                            break;
+                    }
+                    telefone = JOptionPane.showInputDialog(null, "Informe seu Número:");
+                        if (telefone == null) {
+                            break;                        
+                        }
+                    enderecoCliente = JOptionPane.showInputDialog(null, "Informe seu endereço:");   
+                        if (enderecoCliente == null) {
+                            break;                        
+                        }
+                String MenssagemConfirmacao = "Confirme suas informações:\n" +
+                                                "Nome: " + nomeCliente + "\n" +
+                                                "telefone: " + telefone + "\n" + 
+                                                "Endereço :" + enderecoCliente;   
+                int escolha = JOptionPane.showConfirmDialog(null, MenssagemConfirmacao, "Confirmação", JOptionPane.YES_NO_OPTION);
+                    if (escolha == JOptionPane.YES_OPTION) {
+                        JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                        cadastrado = true; // Para sair do loop após o sucesso
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Cadastro cancelado. Por favor, insira as informações novamente.", "Aviso", JOptionPane.WARNING_MESSAGE);
+                    }
 
-    } 
-}
+                 }
+
+                 JOptionPane.showMessageDialog(null, " Informe o tamanho da área do seu jardim: ");
+
+            Double largura =  Double.parseDouble(JOptionPane.showInputDialog(null, "Informe a Largura: "));
+            Double comprimento = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o Comprimento: " ));
+                 
+            Double area = largura * comprimento;
+            
+                JOptionPane.showMessageDialog(null, "A área do jardim é: " + area + " m²");
+
+        } 
+    }
+
+
