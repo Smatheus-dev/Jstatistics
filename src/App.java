13eb51d0256d250e9897a6c920d78559b223f6ef
Smatheus-dev <m.t.s_@hotmail.com>
2025-05-09 08:30:09 -0300

    Mandando aula6 para o Git

--- src/App.java
+++ src/App.java
@@ -1,5 +1,28 @@
+import javax.swing.JOptionPane;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Alô Git!");
-    }
+
+        
+                    //  Classe é um conjunto de objetos
+                    //  Nosso objetivo nesse código é entender a diferença entre método com o tipo VOID e com o tipo String 
+                    // Entre método com o tipo void e método com o o tipo String
+                    Mamifero m1 = new Mamifero();
+                    m1.setNomeCientifico("Bos Taurus");
+                    JOptionPane.showMessageDialog(null, "Nome Cientifico"+ "\n" + m1.getNomeCientifico());
+
+                    Mamifero m2 = new Mamifero();
+                    m2.setNomeCientifico("Macropus");
+                    JOptionPane.showMessageDialog(null, "Nome Cientifico"+ "\n" + m2.getNomeCientifico());
+
+                    Mamifero m3 = new Mamifero();
+                    m3.setNomeCientifico("Panthera tigris");
+                    JOptionPane.showMessageDialog(null,  "Nome Cientifico"+ "\n" + m3.getNomeCientifico());
+                    
+                    Mamifero m4 = new Mamifero();
+                    m4.setNomeCientifico("Loxodonta africana");
+                    JOptionPane.showMessageDialog(null,  "Nome Cientifico"+ "\n" + m4.getNomeCientifico());
+
+    } 
 }
