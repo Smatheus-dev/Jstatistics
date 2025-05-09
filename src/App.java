236da1e33e80f9e992528ad0890ebfa34ec8171e
Smatheus-dev <m.t.s_@hotmail.com>
2025-05-09 11:58:36 -0300

    Começo do exercício 2

--- src/App.java
+++ src/App.java
@@ -1,4 +1,4 @@
-  
+ //   ------------------------------------------------Ex 1 ------------------------------------------------------------------------------ 
     import javax.swing.JOptionPane;
 
 
@@ -16,28 +16,28 @@
                     JOptionPane.showMessageDialog(null, " Por favor, informe seus dados: "); 
                     
                     nomeCliente = JOptionPane.showInputDialog(null, "Informe seu nome:");
-                        if (nomeCliente == null){
-                            break;
-                    }
-                    telefone = JOptionPane.showInputDialog(null, "Informe seu Número:");
-                        if (telefone == null) {
-                            break;                        
-                        }
-                    enderecoCliente = JOptionPane.showInputDialog(null, "Informe seu endereço:");   
-                        if (enderecoCliente == null) {
-                            break;                        
-                        }
+                                if (nomeCliente == null){
+                                    break;
+                                 }
+                            telefone = JOptionPane.showInputDialog(null, "Informe seu Número:");
+                                if (telefone == null) {
+                                    break;                        
+                                }
+                            enderecoCliente = JOptionPane.showInputDialog(null, "Informe seu endereço:");   
+                                if (enderecoCliente == null) {
+                                    break;                        
+                                }
                 String MenssagemConfirmacao = "Confirme suas informações:\n" +
                                                 "Nome: " + nomeCliente + "\n" +
                                                 "telefone: " + telefone + "\n" + 
                                                 "Endereço :" + enderecoCliente;   
                 int escolha = JOptionPane.showConfirmDialog(null, MenssagemConfirmacao, "Confirmação", JOptionPane.YES_NO_OPTION);
-                    if (escolha == JOptionPane.YES_OPTION) {
-                        JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
-                        cadastrado = true; // Para sair do loop após o sucesso
-                    } else {
-                        JOptionPane.showMessageDialog(null, "Cadastro cancelado. Por favor, insira as informações novamente.", "Aviso", JOptionPane.WARNING_MESSAGE);
-                    }
+                            if (escolha == JOptionPane.YES_OPTION) {
+                                JOptionPane.showMessageDialog(null, "Cadastrado com Sucesso!", "Sucesso", JOptionPane.INFORMATION_MESSAGE);
+                                cadastrado = true; // Para sair do loop após o sucesso
+                            } else {
+                                JOptionPane.showMessageDialog(null, "Cadastro cancelado. Por favor, insira as informações novamente.", "Aviso", JOptionPane.WARNING_MESSAGE);
+                            }
 
                  }
 
@@ -46,11 +46,32 @@
             Double largura =  Double.parseDouble(JOptionPane.showInputDialog(null, "Informe a Largura: "));
             Double comprimento = Double.parseDouble(JOptionPane.showInputDialog(null, "Informe o Comprimento: " ));
                  
-            Double area = largura * comprimento;
-            
+            Double area = largura * comprimento;        
                 JOptionPane.showMessageDialog(null, "A área do jardim é: " + area + " m²");
 
+ //   ------------------------------------------------Ex 2 ------------------------------------------------------------------------------           
+            String classificacao;
+                        if (area >= 100 ) {
+                            classificacao = "Grande";  
+                        }else{
+                            classificacao = "Pequeno";
+                        }
+                 JOptionPane.showMessageDialog(null, "Classificação do jardim: " + classificacao, "Classificação", JOptionPane.INFORMATION_MESSAGE);
+
+
+
+                String[] opcoesService = {
+                            "1 - Manutenção de Jardins",
+                            "2 - Paisagismo e Projetos de Jardim",
+                            "3 - Instalação de Sistema de Irrigação",
+                            "4 - Plantio de Horta e Jardim Vertical",
+                            "5 - Controle de Pragas e Doenças"
+                };
+
+                        
         } 
     }
 
 
+     
+    
